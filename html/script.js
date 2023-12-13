d9ca9d0beafa828950b650a1f9a5a47fafbe3c51
lidiya207 <lidiyagetale@icloud.com>
2023-12-13 10:10:23 -0800

    First commit

---
+++ html/script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    function toggleNewsletter() {
+       const newsLetter = document.getElementById("newsletter");
+       newsLetter.classList.toggle("active");
+    }
+ });
